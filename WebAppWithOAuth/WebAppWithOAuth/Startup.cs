46488b1e80eb28c3770316fb901f22cd0cbc6eef
iqans <iqan.shaikh@gmail.com>
2016-08-01 11:05:33 +0100

    init

---
+++ WebAppWithOAuth/WebAppWithOAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppWithOAuth.Startup))]
+namespace WebAppWithOAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
